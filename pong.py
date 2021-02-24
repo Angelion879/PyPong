b586ae295858b20f7538a5aefd244cdfa6d1088e
Angelis <48072083+Angelion879@users.noreply.github.com>
2021-02-23 22:07:35 -0300

    Paddles movement update

--- pong.py
+++ pong.py
@@ -30,6 +30,33 @@ right_paddle = Paddle((360,0))
 ball = Ball((0,0))
 
 # Functions
+def left_paddle_up():
+    y = left_paddle.ycor()
+    y += 20
+    left_paddle.sety(y)
+
+def right_paddle_up():
+    y = right_paddle.ycor()
+    y += 20
+    right_paddle.sety(y)
+
+def left_paddle_down():
+    y = left_paddle.ycor()
+    y -= 20
+    left_paddle.sety(y)
+
+def right_paddle_down():
+    y = right_paddle.ycor()
+    y -= 20
+    right_paddle.sety(y)
+
+# Key binding
+wind.listen()
+wind.onkeypress(left_paddle_up, 'w')
+wind.onkeypress(right_paddle_up, 'Up')
+wind.onkeypress(left_paddle_down, 's')
+wind.onkeypress(right_paddle_down, 'Down')
+
 
 # Main game loop
 while True:
